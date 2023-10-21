bbaece2510a3ce459db9cc341c7081478259dde3
Leon Aiossa <guide_X@live.com>
2023-10-20 21:31:08 -0700

    12345

---
+++ nexIRC.Business/Helper/ConstantsHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace nexIRC.Business.Helper {
+    /// <summary>
+    /// Constants Helper
+    /// </summary>
+    public static class ConstantsHelper {
+        /// <summary>
+        /// CRLF
+        /// </summary>
+        public const string CrLf = "\r\n";
+    }
+}
---
+++ nexIRC.Model/QueryModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+namespace nexIRC.Model {
+    /// <summary>
+    /// Query
+    /// </summary>
+    public class QueryModel {
+        /// <summary>
+        /// User
+        /// </summary>
+        public UserModel User { get; }
+        /// <summary>
+        /// Nick
+        /// </summary>
+        public string Nick => User.Nick;
+        /// <summary>
+        /// Messages
+        /// </summary>
+        public ObservableCollection<QueryMessageModel> Messages { get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="user"></param>
+        public QueryModel(UserModel user) {
+            User = user;
+            Messages = new ObservableCollection<QueryMessageModel>();
+        }
+    }
+}
